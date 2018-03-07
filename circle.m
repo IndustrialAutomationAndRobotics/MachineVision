a6786fd6610ab471eb31f04c82ed95bccdefcdd9
HazimGharib <hazimgharib21@gmail.com>
2018-03-07 09:59:15 +0800

    update circle.m

--- circle.m
+++ circle.m
@@ -1,6 +1,6 @@
 
 % Read the input vedio and get only the first frame
-vidInput = VideoReader('E:\Parasite\Documents\py\MachineVision\circle.mov');
+vidInput = VideoReader('C:\Users\lab E211\Desktop\circle.mov');
 images = read(vidInput, 1);
 
 % Show it and change it to grayscale for segmentation method
@@ -33,7 +33,7 @@ BWnobord = imclearborder(BWdfill, 4);
 % We erode it back to get back the actual size and remove some noise
 seD = strel('diamond', 1);
 BWfinal = imerode(BWnobord,seD);
-%figure, imshow(BWfinal), title('segmented image');
+figure, imshow(BWfinal), title('segmented image');
 
 % Shape Based Feature Extraction part
 [row, column] = size(BWfinal);
@@ -114,3 +114,100 @@ rMean = round(sum(rowWeight(:))/num_motion);
 
 a1 = sum(rowWeight(:).^2);
 a2 = (sum(rowWeight(:))^2)/num_motion;
+rStd = round(sqrt((a1-a2)/(num_motion-1)));
+
+std_weight = 2.2;
+rStd = rStd*std_weight;
+cStd = cStd*std_weight;
+test = cMean + min(cStd, ceil(column/2));
+ColumnStart = max(1, cMean - min(cStd, ceil(column/2)));
+ColumnEnd = min(column, cMean + min(cStd, ceil(column/2)));
+RowStart = max(1, rMean - min(rStd, ceil(row/2)));
+RowEnd = min(row, rMean + min(rStd, ceil(row/2)));
+
+% Draw the lines of the row and column
+line([ColumnStart, ColumnEnd], [RowStart, RowStart], 'Color','blue','Linewidth',2);
+line([ColumnStart, ColumnEnd], [RowEnd, RowEnd], 'Color','blue','Linewidth',2);
+line([ColumnStart, ColumnStart], [RowStart, RowEnd], 'Color','blue','Linewidth',2);
+line([ColumnEnd, ColumnEnd], [RowStart, RowEnd], 'Color','blue','Linewidth',2);
+%drawnow;
+
+% crop image
+Y=imcrop(BWfinal,[ColumnStart, RowStart,ColumnEnd-(ColumnStart),RowEnd-(RowStart)]);
+[row,column] = size(Y);
+Y = double(Y);
+
+%figure, imshow(Y);
+
+stats = regionprops(Y, 'basic');
+Y1=stats(1).Centroid;
+Y1=round(Y1);
+xc = Y1(1); yc=Y1(2);
+test = Y(:,:);
+
+% loop dari centroid horizontally ke max column.
+% nilai h1 akan simpan position pixel bila nilai pixel = 1
+h1 = 0; bil = 1;
+for i = xc:column
+    if Y(yc,i)==1
+        h1=i;
+    else
+        if h1<=0
+            h1=1;
+        end
+    end
+end
+s0 = [h1 yc];
+
+% loop dari min row vertically hingga ke centroid
+% nilai v1 akan increment bila nilai pixel = 0
+v1 = 0;
+for i = 1:yc
+    if Y(i,xc)==0
+        v1=i+1;
+    else
+        if v1 <= 0
+            v1 = 1;
+        end
+    end
+    disp(v1);
+end
+s9=[xc v1];
+
+% loop dari min column horizontaly hingga ke centroid
+% nilai h2 akan increment bila nilai pixel = 0
+h2 = 0;
+for i = 1:xc
+    if Y(yc, i)==0
+        h2=i+1;
+    else
+        if h2<=0
+            h2=1;
+        end
+    end
+end
+s18=[h2 yc];
+
+% loop dari centroid vertically hingga ke max row
+% nilai v2 akan simpan position pixel bila nilai pixel = 1
+v2 = 0;
+for i=yc:row
+    if Y(i,xc)==1
+        v2=i;
+    else
+        if v2<=0
+            v2=1;
+        end
+    end
+end
+s27=[xc,v2];
+
+% uncomment kalo nak tengok result dari coding atas tu
+%imshow(Y);
+%hold on;
+%plot(h1,yc,'r*','LineWidth',2,'MarkerSize', 10);
+%plot(xc,v1,'r*','LineWidth',2,'MarkerSize', 10);
+%plot(h2,yc,'r*','LineWidth',2,'MarkerSize', 10);
+%plot(xc,v2,'r*','LineWidth',2,'MarkerSize', 10);
+
+A=0;B=0;C=0;D=0;
